14b1532e12a721ac3258191d2e865f2f5eb7d541
Aman Ganapathy <amanganapathy@gmail.com>
2022-12-20 23:12:28 -0800

    Added functionality in index.html to accept new notices which sends Post request to server.js that produces newNotice object.

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,39 @@
 //Module imports
 var express = require('express')
 var path = require('path')
+var bodyParser = require('body-parser')
 
 //constant declarations
 const mainPage = "index.html";
 
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.use(express.static('frontend'))
 app.get('/',(request,response)=>{
     console.log("Get request to homepage received.")
     response.sendFile(path.resolve(`../frontend/${mainPage}`))
 })
-app.post('/new_notice',(request,response)=>{
+app.post('/new_notice',urlencodedParser,(request,response)=>{
     console.log("POST REQUEST RECEIVED")
-    response.send("NEW FILE RECEIVED")
+    var priorityClass = '';
+    if(request.body.hPriority == 'on')
+    {
+        priorityClass = 'high';
+    }
+    else if(request.body.mPriority == 'on')
+    {
+        priorityClass = 'medium';
+    }
+    else{
+        priorityClass = 'low';
+    }
+    newNotice= {
+        noticeName : request.body.noticeName,
+        dueDate    : request.body.dueDate,
+        priority   : priorityClass
+    };
+    response.send(JSON.stringify(newNotice))
 })
 var server = app.listen(8081,()=>{
     var host = server.address().address;
